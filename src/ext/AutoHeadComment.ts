a2bb2571b0cc81600b18d9ceb5a03269e416bde8
baimingjiang <baimingjiang@topjoy.com>
2019-05-16 10:26:45 +0800

    插入快捷键

--- src/ext/AutoHeadComment.ts
+++ src/ext/AutoHeadComment.ts
@@ -2,44 +2,35 @@ import * as vscode from 'vscode';
 import {Util} from '../utils/Util';
 
 export class AutoHeadComment {
+    private static splitLineStrForLua : string = "--==============================--";
+
     public static init(context: vscode.ExtensionContext) {
         vscode.workspace.onDidChangeTextDocument(event => {
-            if(this.checkComment(event)){
-                
-            }
+            if(this.insertComment(event)){ }
         })
 
-        let disposable = vscode.commands.registerCommand('extension.insertautoheadcomment', () => {
-            vscode.window.showInformationMessage('Hello World!');
-            var editor = vscode.window.activeTextEditor as vscode.TextEditor;
-            var document = editor.document;
-
-            var range: vscode.Range = new vscode.Range(
-                        new vscode.Position(0, 0),
-                        new vscode.Position(document.lineCount, 10000));
-            var text = document.getText(range);
-
-            for (var i = 1; i < document.lineCount; i ++) {
-                var textLine = document.lineAt(i);
-                var str = textLine.text;
-                if (str === "--==============================--") {
-                    var insertLine = i - 1;
-                    editor.edit(function (edit) {
-                        var insertStr : string = "    --@modify:" + Util.getInstance().getAuthor() +  " at " + Util.getInstance().getCurDate() + "\n";
-                        edit.insert(new vscode.Position(insertLine, 0), insertStr);
-                    });
-                }
-            }
-
+        let disposable = vscode.commands.registerCommand('extension.insertmodifycomment', () => {
+            vscode.window.showInformationMessage('Insert modified comments by ' + Util.getInstance().getAuthor());
+            this.insertModifyComment()
         });
-    
         context.subscriptions.push(disposable);
+
+        let disposable1 = vscode.commands.registerCommand("extension.insertautoheadcomment", () => {
+            vscode.window.showInformationMessage('Insert head comments by ' + Util.getInstance().getAuthor());
+            this.insertAutoHeadComment()
+        });
+        context.subscriptions.push(disposable1);
     }
 
-    private static checkComment(event: vscode.TextDocumentChangeEvent): Boolean {
+    private static insertComment(event: vscode.TextDocumentChangeEvent): Boolean {
         var ret : Boolean = false;
 
         var document = event.document;
+        var editor = vscode.window.activeTextEditor as vscode.TextEditor;
+
+        if (editor === null) {
+            return false;
+        }
 
         var curentLine = event.contentChanges[0].range.start.line;
         var lineText = document.lineAt(curentLine).text;
@@ -54,9 +45,11 @@ export class AutoHeadComment {
                         // var text = document.getText(range);
 
                         var insterText = this.createLuaHeadCommentString(document);
-                        var editor = vscode.window.activeTextEditor as vscode.TextEditor;
                         editor.edit(function (edit) {
-                            edit.insert(event.contentChanges[0].range.start, insterText);
+                            // edit.delete(event.contentChanges[0].range);
+                            // edit.insert(new vscode.Position(document.lineAt(curentLine).lineNumber, 0), insterText);
+
+                            edit.replace(document.lineAt(curentLine).range, insterText);
                         });
                     }
                 }
@@ -70,7 +63,7 @@ export class AutoHeadComment {
                         var insterText = this.createCppHeadCommentString(document);
                         var editor = vscode.window.activeTextEditor as vscode.TextEditor;
                         editor.edit(function (edit) {
-                            edit.insert(event.contentChanges[0].range.start, insterText);
+                            edit.replace(document.lineAt(curentLine).range, insterText);
                         });
                     }
                 }
@@ -80,17 +73,55 @@ export class AutoHeadComment {
         return ret;
     }
 
+    private static insertAutoHeadComment() {
+        var editor = vscode.window.activeTextEditor as vscode.TextEditor;
+        
+        if (editor) {
+            var document = editor.document;
+            var insterText = this.createLuaHeadCommentString(document);
+
+            editor.edit(function (edit) {
+                edit.insert(new vscode.Position(0, 0), insterText);
+            });
+        }
+    }
+
+    private static insertModifyComment() {
+        var editor = vscode.window.activeTextEditor as vscode.TextEditor;
+        if (editor) {
+            var document = editor.document;
+
+            var range: vscode.Range = new vscode.Range(
+                        new vscode.Position(0, 0),
+                        new vscode.Position(document.lineCount, 10000));
+            var text = document.getText(range);
+    
+            for (var i = 1; i < document.lineCount; i ++) {
+                var textLine = document.lineAt(i);
+                var str = textLine.text;
+                if (str === this.splitLineStrForLua) {
+                    var insertLine = i - 1;
+                    editor.edit(function (edit) {
+                        var insertStr : string = "    --@modify:" + Util.getInstance().getAuthor() +  " at " + Util.getInstance().getCurDate() + "\n";
+                        edit.insert(new vscode.Position(insertLine, 0), insertStr);
+                    });
+                }
+            }
+        }
+    }
+
     private static createLuaHeadCommentString(document : vscode.TextDocument) {
         
         var tabStr : string = "    --";
-        
-        var ret = "==============================--\n";
+        var ret : string = "";
+
+        ret += this.splitLineStrForLua + "\n";
         ret += tabStr+"@file: " + Util.getInstance().getFileName(document) + "\n";
         ret += tabStr+"@desc: \n";
         ret += tabStr+"@time: "+ Util.getInstance().getCurDate() + "\n";
         ret += tabStr+"@autor: " + Util.getInstance().getAuthor() + "\n";
         ret += tabStr+"@return \n";
-        ret += "--==============================-";
+        ret += this.splitLineStrForLua;
         
         return ret;
     }
@@ -98,15 +129,22 @@ export class AutoHeadComment {
     private static createCppHeadCommentString(document : vscode.TextDocument) {
         var tabStr : string = "    "
 
-        var ret = "\n==============================\n";
+        var ret = "\n/******************************\n";
         ret += tabStr+"@file: " + Util.getInstance().getFileName(document) + "\n";
         ret += tabStr+"@desc: \n";
         ret += tabStr+"@time: "+ Util.getInstance().getCurDate() + "\n";
         ret += tabStr+"@autor: " + Util.getInstance().getAuthor() + "\n";
         ret += tabStr+"@return \n";
-        ret += "==============================\n*";
+        ret += "******************************\n*/";
         
         return ret;
+    }
 
+    private static createModifyCommentString() {
+        var ret : string = "";
+
+        ret = "    --@modify:" + Util.getInstance().getAuthor() +  " at " + Util.getInstance().getCurDate() + "\n";
+
+        return ret;
     }
 }
